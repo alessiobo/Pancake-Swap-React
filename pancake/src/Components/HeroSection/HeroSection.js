d581a77265edf6527d3171bca327149b102be8bf
Cipriano Mereu <ciprianomereu@gmail.com>
2023-03-28 15:08:04 +0200

    add:buttons

--- pancake/src/Components/HeroSection/HeroSection.js
+++ pancake/src/Components/HeroSection/HeroSection.js
@@ -1,51 +1,55 @@
+import Button from "../UI/Buttons/Button";
 import "./heroSection.css";
+// import img from "./images/astronaut-bunny.webp";
 import HeroSectionBanner from "./HeroSectionBanner/HeroSectionBanner";
 
 function HeroSection() {
   return (
-    <section id="hero-section" class="hero-section">
-      <div class="hero-section-content">
+    <section id="hero-section" className="hero-section">
+      <div className="hero-section-content">
         <HeroSectionBanner />
 
-        <div class="hero-homepage">
-          <div class="hero-homepage-text">
-            <h1 class="hero-homepage-text-title">
+        <div className="hero-homepage">
+          <div className="hero-homepage-text">
+            <h1 className="hero-homepage-text-title">
               The moon is made of pancakes.
             </h1>
-            <p class="hero-homepage-text-parag">
+            <p className="hero-homepage-text-parag">
               Trade, earn, and win crypto on the most popular decentralized
               platform in the galaxy.
             </p>
-            <div class="hero-homepage-buttons">
-              <button class="btn">Connect Wallet</button>
-              <button class="btn-thin">Trade Now</button>
+            <div className="hero-homepage-buttons">
+              <Button type={"btn"}>Connect Wallet</Button>
+              <Button type="btn-thin">Trade Now</Button>
             </div>
           </div>
-          <div class="hero-homepage-images">
+          <div className="hero-homepage-images">
             <img
-              class="img-anim astronaut-bunny-img"
-              src="./Components/HeroSection/images/astronaut-bunny.webp"
+              className="img-anim astronaut-bunny-img"
+              // src="./Components/HeroSection/images/astronaut-bunny.webp"
+              //   src={img["astronaut-bunny.webp"]}
+              //   src={img}
               alt=""
             />
             <img
-              class="astronaut-bunny-img-star hero-anim-star-2"
+              className="astronaut-bunny-img-star hero-anim-star-2"
               src="./Components/HeroSection/images/star-left.webp"
               alt=""
             />
             <img
-              class="astronaut-bunny-img-star hero-anim-star-5"
+              className="astronaut-bunny-img-star hero-anim-star-5"
               src="./Components/HeroSection/images/star-right.webp"
               alt=""
             />
             <img
-              class="astronaut-bunny-img-star hero-anim-star-3"
+              className="astronaut-bunny-img-star hero-anim-star-3"
               src="./Components/HeroSection/images/star-top-r.webp"
               alt=""
             />
           </div>
         </div>
       </div>
-      <div class="hero-bg-wave">
+      <div className="hero-bg-wave">
         <img
           id="hero-wave"
           src="./Components/HeroSection/images/wave-bg.svg"
--- pancake/src/Components/HeroSection/HeroSectionBanner/HeroSectionBanner.js
+++ pancake/src/Components/HeroSection/HeroSectionBanner/HeroSectionBanner.js
@@ -1,44 +1,29 @@
+import "./heroSectionBanner.css";
+import Button from "../../UI/Buttons/Button";
+
 function HeroSectionBanner() {
   return (
-    <div id="hero-slideBar" class="hero-slideBar">
-      <div class="hero-slideBar-content">
+    <div id="hero-slideBar" className="hero-slideBar">
+      <div className="hero-slideBar-content">
         <span>PancakeSwap x Trust Wallet</span>
         <h2>Trade and win $10,000 Prize Pool</h2>
-        <div class="hero-slideBar-content-img"></div>
-        <div class="hero-slideBar-buttons">
-          <button class="btn">
-            <div>
-              <span>Trade now</span>
-              <span>
-                <img
-                  class="hero-slideBar-buttons-icons hero-icon"
-                  src="./Components/HeroSection/images/Icons/icon-arr-r.svg"
-                  alt="icon-arrow-right"
-                />
-              </span>
-            </div>
-          </button>
-          <button class="btn">
-            <div>
-              <span>Download</span>
-              <span>
-                <img
-                  class="hero-icon"
-                  src="./Components/HeroSection/images/Icons/icon-arrow-up.svg"
-                  alt="icon-arrow-up"
-                />
-              </span>
-            </div>
-          </button>
+        <div className="hero-slideBar-content-img"></div>
+        <div className="hero-slideBar-buttons">
+          <Button type="btn" img={"arrR"}>
+            Trade now
+          </Button>
+          <Button type="btn-invert" img={"arrUP"}>
+            Download
+          </Button>
         </div>
-        <div class="hero-slideBar-btnSlider">
+        <div className="hero-slideBar-btnSlider">
           <span id="slider1"></span>
           <span id="slider2"></span>
           <span id="slider3"></span>
         </div>
-        <div class="hero-slideBar-img">
+        <div className="hero-slideBar-img">
           <img
-            class="hero-slideBar-img-bunny1"
+            className="hero-slideBar-img-bunny1"
             src="./Components/HeroSection/images/hero-slidebar-bunny1.webp"
             alt="hero-slidebar"
           />
