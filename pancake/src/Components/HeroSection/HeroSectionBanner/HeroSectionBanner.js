a3f82a8ba70cb5d9ffe5ac2112edc6249d889dd1
Cipriano Mereu <94009419+GitAbbonu@users.noreply.github.com>
2023-03-30 12:48:24 +0200

    Hero section (#6)
    * change btn
    
    * fix:slider buttons
    
    * fix:slider buttons
    
    * fix:z-index
    
    * fix:z-index
    
    * add:animation
    
    ---------
    
    Co-authored-by: Cipriano Mereu <ciprianomereu@gmail.com>

--- pancake/src/Components/HeroSection/HeroSectionBanner/HeroSectionBanner.js
+++ pancake/src/Components/HeroSection/HeroSectionBanner/HeroSectionBanner.js
@@ -1,3 +1,4 @@
+import "animate.css";
 import "./heroSectionBanner.css";
 import Button from "../../UI/Buttons/Button";
 
@@ -5,60 +6,78 @@ import Button from "../../UI/Buttons/Button";
 import bunny1 from "../images/hero-slidebar-bunny-1.webp";
 import bunny2 from "../images/hero-slidebar-bunny-2.webp";
 import bunny3 from "../images/hero-slidebar-bunny-3.webp";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { useEffect } from "react";
 
 function HeroSectionBanner() {
+  const bannerRef = useRef();
+
   const [slide, setSlide] = useState(1);
 
   const [banner, setBanner] = useState(1);
 
   const sliderClickHandler = useCallback((num) => {
-    setSlide(num);
+    bannerRef.current.className =
+      "hero-slideBar animate__animated animate__bounceOutLeft";
+
+    setTimeout(() => {
+      bannerRef.current.className =
+        "hero-slideBar animate__animated animate__bounceInRight";
+      setSlide(num);
+    }, 350);
   }, []);
 
-  const banner1 = {
-    subTitle: "PancakeSwap x Trust Wallet",
-    title: "Trade and win $10,000 Prize Pool",
-    buttons: [
-      {
-        type: "btn",
-        img: "arrR",
-        bntTitle: "Trade now",
+  const banner1 = useMemo(() => {
+    return {
+      id: 1,
+      subTitle: "PancakeSwap x Trust Wallet",
+      title: "Trade and win $10,000 Prize Pool",
+      buttons: [
+        {
+          type: "btn",
+          img: "arrR",
+          bntTitle: "Trade now",
+        },
+        {
+          type: "btn-invert",
+          img: "arrUP",
+          bntTitle: "Download",
+        },
+      ],
+      img: bunny1,
+      styleBG: {
+        background:
+          "radial-gradient(104.12% 231.19% at -4.12% -5.83%, rgb(0, 87, 174) 0.52%,rgb(102, 124, 198) 67.41%,rgb(25, 229, 245) 100%)",
       },
-      {
-        type: "btn-invert",
-        img: "arrUP",
-        bntTitle: "Download",
+    };
+  }, []);
+
+  const banner2 = useMemo(() => {
+    return {
+      id: 2,
+      subTitle: "PancakeSwap x APTOS",
+      title: "Hello Aptos Project Teams!",
+      buttons: [{ type: "btn", img: "arrUP", bntTitle: "👋 Get in Touch!" }],
+      img: bunny2,
+      styleBG: {
+        background: "linear-gradient(rgb(0, 191, 165) 0%, rgb(0, 90, 90) 100%)",
       },
-    ],
-    img: bunny1,
-    styleBG: {
-      background:
-        "radial-gradient(104.12% 231.19% at -4.12% -5.83%, rgb(0, 87, 174) 0.52%,rgb(102, 124, 198) 67.41%,rgb(25, 229, 245) 100%)",
-    },
-  };
+    };
+  }, []);
 
-  const banner2 = {
-    subTitle: "LOL",
-    title: "Hello Aptos Project Teams!",
-    buttons: [{ type: "btn", img: "arrUP", bntTitle: "👋 Get in Touch!" }],
-    img: bunny2,
-    styleBG: {
-      background: "linear-gradient(rgb(0, 191, 165) 0%, rgb(0, 90, 90) 100%)",
-    },
-  };
-
-  const banner3 = {
-    subTitle: "Perpetual Futures",
-    title: "UP TO 100x LEVERAGE",
-    buttons: [{ type: "btn", img: "arrR", bntTitle: "Trade now" }],
-    img: bunny3,
-    styleBG: {
-      background:
-        "-webkit-linear-gradient(top, rgb(118, 69, 217) 0%, rgb(69, 42, 122) 100%)",
-    },
-  };
+  const banner3 = useMemo(() => {
+    return {
+      id: 3,
+      subTitle: "Perpetual Futures",
+      title: "UP TO 100x LEVERAGE",
+      buttons: [{ type: "btn", img: "arrR", bntTitle: "Trade now" }],
+      img: bunny3,
+      styleBG: {
+        background:
+          "-webkit-linear-gradient(top, rgb(118, 69, 217) 0%, rgb(69, 42, 122) 100%)",
+      },
+    };
+  }, []);
 
   useEffect(() => {
     switch (slide) {
@@ -77,11 +96,15 @@ function HeroSectionBanner() {
   }, [slide]);
 
   return (
-    <div style={banner.styleBG} className="hero-slideBar">
+    <div
+      ref={bannerRef}
+      style={banner.styleBG}
+      className="hero-slideBar animate__animated animate__bounce"
+    >
       <div className="hero-slideBar-content">
         <span>{banner.subTitle}</span>
         <h2>{banner.title}</h2>
-        {/* <div className="hero-slideBar-content-img"></div> */}
+
         <div className="hero-slideBar-btn">
           {banner.buttons?.map((el, k) => {
             return (
@@ -92,9 +115,18 @@ function HeroSectionBanner() {
           })}
         </div>
         <div className="hero-slideBar-btnSlider">
-          <span onClick={() => sliderClickHandler(1)}></span>
-          <span onClick={() => sliderClickHandler(2)}></span>
-          <span onClick={() => sliderClickHandler(3)}></span>
+          <span
+            className={banner.id === 1 ? "slidebarSpan1" : ""}
+            onClick={() => sliderClickHandler(1)}
+          ></span>
+          <span
+            className={banner.id === 2 ? "slidebarSpan2" : ""}
+            onClick={() => sliderClickHandler(2)}
+          ></span>
+          <span
+            className={banner.id === 3 ? "slidebarSpan3" : ""}
+            onClick={() => sliderClickHandler(3)}
+          ></span>
         </div>
         <div className="hero-slideBar-img">
           <img
