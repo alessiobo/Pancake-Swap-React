024bbce8b1a309032b989c80fbe8bf46e906e442
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-03-30 03:05:57 +0200

    nuovo

--- pancake/src/Components/Pages/HomePage.js
+++ pancake/src/Components/Pages/HomePage.js
@@ -1,10 +1,12 @@
 import HeroSection from "../HeroSection/HeroSection";
+import BuyCake from "../buyCake-section/buyCake-section";
 
 function HomePage() {
   return (
     <div>
       HomePage SUCA
       <HeroSection />
+      <BuyCake />
     </div>
   );
 }
