bbb09f14588c40727f3c6cae534678c10d19a0be
Alessio Boscaro <119036817+alessiobo@users.noreply.github.com>
2023-03-31 13:02:43 +0200

    add twin-section

--- pancake/src/Components/Pages/HomePage.js
+++ pancake/src/Components/Pages/HomePage.js
@@ -1,10 +1,12 @@
 import HeroSection from "../HeroSection/HeroSection";
+import TwinSection from "../twin-section/TwinSection";
 
 function HomePage() {
   return (
     <div>
       HomePage SUCA
       <HeroSection />
+      <TwinSection />
     </div>
   );
 }
--- pancake/src/twin-section/Components/TwinSection.js
+++
@@ -1,2 +0,0 @@
-import React from "react";
-import Section3 from "./Section3";
