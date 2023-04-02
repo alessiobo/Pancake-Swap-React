bcca38123bbeca3a47cbc78ec225d9c262b3829f
Roberta <federicoroberta1@gmail.com>
2023-04-02 21:06:08 +0200

    card-sec in HomePage

--- pancake/src/Components/Pages/HomePage.js
+++ pancake/src/Components/Pages/HomePage.js
@@ -1,12 +1,14 @@
 import HeroSection from "../HeroSection/HeroSection";
 import BuyCake from "../buyCake-section/buyCake-section";
 import TwinSection from "../twin-section/TwinSection";
+import CardSection from "../CardSection/Card-Section";
 
 function HomePage() {
   return (
     <div>
       HomePage SUCA
       <HeroSection />
+      <CardSection />
       <TwinSection />
       <BuyCake />
     </div>
