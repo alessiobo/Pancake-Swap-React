af8591ec01c4eda5b9e2018bfc01a3ed94a59f30
Cipriano Mereu <ciprianomereu@gmail.com>
2023-04-02 21:31:16 +0200

    add:navbar-footer

--- pancake/src/Components/Pages/HomePage.js
+++ pancake/src/Components/Pages/HomePage.js
@@ -6,7 +6,6 @@ import CardSection from "../CardSection/Card-Section";
 function HomePage() {
   return (
     <div>
-      HomePage SUCA
       <HeroSection />
       <CardSection />
       <TwinSection />
