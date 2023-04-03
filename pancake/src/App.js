88cb24d9be6e1b72edd76350dd2d6f61eedd9c36
Cipriano Mereu <ciprianomereu@gmail.com>
2023-04-03 20:19:58 +0200

    add:dark mode to heroSection

--- pancake/src/App.js
+++ pancake/src/App.js
@@ -1,22 +1,26 @@
 import { Route, Routes } from "react-router-dom";
+import { contextDarkMode } from "./Components/UI/DarkMode/contextDarkMode";
+
 import HomePage from "./Components/Pages/HomePage";
 import SwapPage from "./Components/Pages/SwapPage";
 import LimitPage from "./Components/Pages/LimitPage";
-import LiquidityPage from "./Components/Pages/LiquidityPage"
+import LiquidityPage from "./Components/Pages/LiquidityPage";
 import Footer from "./Components/Footer/Footer";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/swap" element={<SwapPage/>}/>
-        <Route path="/limit" element={<LimitPage/>}/>
-        <Route path="/liquidity" element={<LiquidityPage/>}/>
-      </Routes>
-      <Footer/>
+      <contextDarkMode.Provider value={true}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/swap" element={<SwapPage />} />
+          <Route path="/limit" element={<LimitPage />} />
+          <Route path="/liquidity" element={<LiquidityPage />} />
+        </Routes>
+        <Footer />
+      </contextDarkMode.Provider>
     </div>
   );
 }
