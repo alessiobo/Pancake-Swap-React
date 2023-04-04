b1582d80365a390d14d8bc5785775e3361968664
Cipriano Mereu <ciprianomereu@gmail.com>
2023-04-04 19:27:08 +0200

    start nft page

--- pancake/src/Components/Pages/Nft/Nft.js
+++ pancake/src/Components/Pages/Nft/Nft.js
@@ -1,17 +1,29 @@
 import "./nft.css";
 
 import CardCollection from "../../UI/Cards/CardCollection/CardCollection";
+import Button from "../../UI/Buttons/Button";
 
 function Nft() {
   return (
-    <div className="nft-container">
-      <CardCollection />
-      <CardCollection />
-      <CardCollection />
-      <CardCollection />
-      <CardCollection />
-      <CardCollection />
-    </div>
+    <section>
+      {/* Navbar */}
+      {/* Title */}
+      <div>
+        <div>
+          <h3>Newest Collections</h3>
+          <Button type={"btn-thin"}>View All</Button>
+        </div>
+
+        <div className="nft-container">
+          <CardCollection />
+          <CardCollection />
+          <CardCollection />
+          <CardCollection />
+          <CardCollection />
+          <CardCollection />
+        </div>
+      </div>
+    </section>
   );
 }
 
