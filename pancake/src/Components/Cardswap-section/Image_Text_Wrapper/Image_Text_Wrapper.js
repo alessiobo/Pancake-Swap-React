f1094798140699b709a83529d918a5ba2cd7c42d
marcobonni <marco.bonni19@gmail.com>
2023-04-04 18:11:09 +0200

    finished

--- pancake/src/Components/Cardswap-section/Image_Text_Wrapper/Image_Text_Wrapper.js
+++ pancake/src/Components/Cardswap-section/Image_Text_Wrapper/Image_Text_Wrapper.js
@@ -1,5 +1,6 @@
 import help_img from "./help.png"
 import "./Image_Text_Wrapper.css"
+import tail_purple from "./tail_purple.svg"
 function Image_text_Wrapper() {
     return (
         <div className="Image_Text_Wrapper">
@@ -7,7 +8,8 @@ function Image_text_Wrapper() {
                 <a href="www.google.com" className="Bridge_assets">Bridge assets to BNB Chain</a>
             </div>
             <div>
-            <a className="need_help" href="google">Need Help?</a>
+            <a className="need_help btn" href="google">Need Help?</a>
+            <img src={tail_purple}></img>
             <img src={help_img} alt="help.png"></img>
             </div>
         </div>
