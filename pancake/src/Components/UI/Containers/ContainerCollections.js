e857a2899400d59b028f26f59a02259bdd8429c8
Cipriano Mereu <ciprianomereu@gmail.com>
2023-04-05 22:16:22 +0200

    fix:cards

--- pancake/src/Components/UI/Containers/ContainerCollections.js
+++ pancake/src/Components/UI/Containers/ContainerCollections.js
@@ -5,7 +5,7 @@ function ContainerCollections(props) {
   return (
     <section>
       <div className="containerCollection-title">
-        <h2>{props.title}</h2>
+        <h2 style={{ color: "#280d5f" }}>{props.title}</h2>
         <Button addStyle={{ height: "32px" }} type={"btn-thin"}>
           View All
         </Button>
