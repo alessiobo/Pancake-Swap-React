1cc0220032ab1d5c2b0bd9b1a03d2953a93fd96b
giovyc93 <118946227+giovyc93@users.noreply.github.com>
2023-04-05 18:28:36 +0200

    fix

--- pancake/src/Components/Pages/LiquidityComponents/Liquidity.js
+++ pancake/src/Components/Pages/LiquidityComponents/Liquidity.js
@@ -6,41 +6,43 @@ import coniglio from "./bunny.webp"
 export const Liquidity = () => {
     return (
         <div className="container">
-            <div className="bunnyCard">
-                <h1>PancakeSwap v3 Migration</h1>
-                <p>Migrate to continue farming CAKE rewards and earning trading fees</p>
-
-
-            </div>
-            <div className="bunnyLiquidBox">
+            <div className="bunnyContent">
+                <div className="bunnyCard">
+                    <div className="firstRowBu">
+                    <h1>PancakeSwap v3 Migration</h1>
+                    </div>
+                    <div className="secondRowLi">
+                    <p>Migrate to continue farming CAKE rewards and earning trading fees</p>
+                    </div>
+                    <div className="buttonLiquidity">
+                    <Button type={"btn"} img={"arrR"}>Proceed</Button>
+                    <Button type={"btn"} img={"arrR"}>Guide</Button>
+                </div>
                 <img
-                    src={coniglio}
-                    className="bunnyLiquid"
+                        src={coniglio}
+                        className="bunnyLiquid"
 
-                />
-            </div>
-            <div className="buttonLiquidity">
-                <Button type={"btn"} img={"arrR"}>Proceed</Button>
-                <Button type={"btn"} img={"arrR"}>Guide</Button>
-            </div>
-
-            <div className="liquidityCard">
-                <div className="firstRowLi">
-                    <h2>Your Liquidity</h2>
-                    <p className="firstRowP">icona</p>
+                    />
                 </div>
-                <div className="secondRowLi">
-                    <p>List of your liquidity positions</p>
+               
+                
+                <div className="liquidityCard">
+                    <div className="firstRowLi">
+                        <h2>Your Liquidity</h2>
+                        <p className="firstRowP">icona</p>
+                    </div>
+                    <div className="secondRowLi">
+                        <p>List of your liquidity positions</p>
+                    </div>
+                    <hr></hr>
+                    <ThirdRowLi />
+                    <div className="fourthRowLi">
+                        <h1>No liquidity found.</h1>
+                    </div>
                 </div>
-                <hr></hr>
-                <ThirdRowLi />
-                <div className="fourthRowLi">
-                <h1>No liquidity found.</h1>
-                </div>
+
+
             </div>
-
-
-
         </div>
 
     )
