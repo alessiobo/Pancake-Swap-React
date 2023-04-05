a9ffba57acad36d55d3e3e6e113ac21d38deedb0
Cipriano Mereu <ciprianomereu@gmail.com>
2023-04-05 18:02:06 +0200

    add: newestCard

--- pancake/src/Components/Pages/Nft/Nft.js
+++ pancake/src/Components/Pages/Nft/Nft.js
@@ -2,18 +2,41 @@ import "./nft.css";
 
 import CardsCollection from "../../CardsCollection/CardsCollection";
 import TitleNFT from "./TitleNFT/TitleNFT";
+import CardNewest from "../../UI/Cards/CardNewest/CardNewest";
+import ContainerCollections from "../../UI/Containers/ContainerCollections";
 
 function Nft() {
   return (
     <section className="nft-container">
       {/* Navbar */}
-      {/* Title */}
+
       <TitleNFT
         title={"NFT Marketplace"}
         subTitle={"Buy and Sell NFTs on BNB Smart Chain"}
       />
       <CardsCollection title={"Newest Collections"} />
       <CardsCollection title={"Hot Collections"} />
+
+      <ContainerCollections title={"Newest Arrivals"}>
+        <div className="newestCollections-container">
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+          <CardNewest />
+        </div>
+      </ContainerCollections>
     </section>
   );
 }
--- pancake/src/Components/UI/Cards/CardCollection/CardCollection.js
+++ pancake/src/Components/UI/Cards/CardCollection/CardCollection.js
@@ -3,6 +3,7 @@ import "./cardCollection.css";
 //images Placheolder
 import banner from "./cardImages/bannerImages/banner-sm.webp";
 import circle from "./cardImages/circleImages/avatar.png";
+import i_vol from "./cardImages/i_volume.svg";
 
 function CardCollection() {
   return (
@@ -13,9 +14,11 @@ function CardCollection() {
         </div>
         <div className="cardCollection-circle">
           <img src={circle} alt="circle_image" />
-          <div className="cardCollection-description">
-            <h4>Lil Pudgys BNB Chain</h4>
-            <h6>Volume 0</h6>
+        </div>
+        <div className="cardCollection-description">
+          <h4>Lil Pudgys BNB Chain</h4>
+          <div>
+            <p>Volume</p> <img src={i_vol} alt="icon" /> <span>100</span>
           </div>
         </div>
       </div>
