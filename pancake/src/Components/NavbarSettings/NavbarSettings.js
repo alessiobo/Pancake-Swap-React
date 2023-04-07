67d900eb3aacf0bd671fc7a9e9c295cc3115646d
Cipriano Mereu <ciprianomereu@gmail.com>
2023-04-07 16:28:48 +0200

    add:darkMode to Settings

--- pancake/src/Components/NavbarSettings/NavbarSettings.js
+++ pancake/src/Components/NavbarSettings/NavbarSettings.js
@@ -3,19 +3,31 @@ import "./navbarSettings.css";
 import x from "./icon-x.svg";
 import Checkbox from "../UI/Checkbox/Checkbox";
 import CheckboxNULL from "../UI/Checkbox/CheckboxNULL";
+import { useContext } from "react";
+import { contextDarkMode } from "../UI/DarkMode/contextDarkMode";
 
 function NavbarSettings({ isHidden, setHidden, mode, setMode }) {
   isHidden
     ? (document.body.style.overflow = "hidden")
     : (document.body.style.overflowY = "visible");
 
+  const darkMode = useContext(contextDarkMode);
+
   return (
     <section
       style={!isHidden ? { display: "none" } : { display: "flex" }}
       class="nav-settings-section"
     >
-      <div class="nav-settings-section-cont">
-        <div class="nav-settings-title">
+      <div
+        class={`nav-settings-section-cont ${
+          darkMode ? "nav-settings-section-cont-dark" : ""
+        }`}
+      >
+        <div
+          class={`nav-settings-title ${
+            darkMode ? "nav-settings-title-dark" : ""
+          }`}
+        >
           <h3>Settings</h3>
           <button
             style={{ background: "transparent", border: 0 }}
@@ -30,17 +42,24 @@ function NavbarSettings({ isHidden, setHidden, mode, setMode }) {
 
           <div className="nav-settings-content">
             <div>
-              <h4>Dark mode</h4>
+              <h4 style={darkMode ? { color: "#fff" } : {}}>Dark mode</h4>
               <Checkbox mode={mode} setMode={setMode} />
             </div>
             <div>
-              <h4>Subgraph Health Indicator</h4> <CheckboxNULL />
+              <h4 style={darkMode ? { color: "#fff" } : {}}>
+                Subgraph Health Indicator
+              </h4>{" "}
+              <CheckboxNULL />
             </div>
             <div>
-              <h4>Show username</h4> <CheckboxNULL />
+              <h4 style={darkMode ? { color: "#fff" } : {}}>Show username</h4>{" "}
+              <CheckboxNULL />
             </div>
             <div>
-              <h4>Token Risk Scanning</h4> <CheckboxNULL />
+              <h4 style={darkMode ? { color: "#fff" } : {}}>
+                Token Risk Scanning
+              </h4>{" "}
+              <CheckboxNULL />
             </div>
           </div>
         </div>
--- pancake/src/Components/UI/Checkbox/CheckboxNULL.js
+++ pancake/src/Components/UI/Checkbox/CheckboxNULL.js
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+import { contextDarkMode } from "../DarkMode/contextDarkMode";
 import "./checkbox.css";
 
 function CheckboxNULL() {
+  const darkMode = useContext(contextDarkMode);
   return (
-    <div class="checkbox-btn">
-      <label class="checkbox-btn-point">
+    <div class={`checkbox-btn ${darkMode ? "checkbox-btn-dark" : ""}`}>
+      <label
+        class={`checkbox-btn-point ${
+          darkMode ? "checkbox-btn-point-dark" : ""
+        }`}
+      >
         <input type="checkbox" class="checkbox-btn-input" />
       </label>
     </div>
