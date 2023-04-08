618630a538a60820599d35170e975f432038ec66
Cipriano Mereu <94009419+GitAbbonu@users.noreply.github.com>
2023-04-08 18:21:50 +0200

    Hero section (#28)
    * change btn
    
    * fix:slider buttons
    
    * fix:slider buttons
    
    * fix:z-index
    
    * fix:z-index
    
    * add:animation
    
    * banner funzionante
    
    * finish
    
    * add:navbar-footer
    
    * some fix add scrollbar
    
    * add:social-icons, checkbox
    
    * fix:footer
    
    * add:dark mode to heroSection
    
    * add:dark mode
    
    * add:dark mode switch
    
    * create:card nft
    
    * start nft page
    
    * add: link Home
    
    * add:ContainerCollection
    
    * fix:ContainerCollection
    
    * add:titleNFT
    
    * add: newestCard
    
    * add: collections newest
    
    * fix:cards
    
    * fix clone
    
    * add:NavbarSettings
    
    * add:darkMode to Settings
    
    * fix: ... bug
    
    * add:404
    
    * add:darkMode to Buttons
    
    ---------
    
    Co-authored-by: Cipriano Mereu <ciprianomereu@gmail.com>

--- pancake/src/Components/UI/Buttons/Button.js
+++ pancake/src/Components/UI/Buttons/Button.js
@@ -2,8 +2,12 @@ import "./button.css";
 
 import iconArrowUP from "./Icons/icon-arrow-up.svg";
 import iconArrowR from "./Icons/icon-arr-r.svg";
+import { contextDarkMode } from "../DarkMode/contextDarkMode";
+import { useContext } from "react";
 
 function Button(props) {
+  const darkMode = useContext(contextDarkMode);
+
   let imgSrc = "";
 
   if (props.img === "arrUP") {
@@ -14,9 +18,24 @@ function Button(props) {
 
   return (
     <button style={props.addStyle} className={props.type}>
-      {props.children}
+      <span
+        style={
+          darkMode && props.type !== "btn-thin" ? { color: "#191326" } : {}
+        }
+      >
+        {props.children}
+      </span>
+
       {imgSrc !== "" && (
         <img
+          style={
+            darkMode
+              ? {
+                  filter:
+                    "invert(8%) sepia(36%) saturate(787%) hue-rotate(217deg) brightness(97%) contrast(99%)",
+                }
+              : {}
+          }
           className={
             props.type === "btn-invert"
               ? "btn-class-img-invert"
