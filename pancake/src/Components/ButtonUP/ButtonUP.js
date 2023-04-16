b740a9d3e88537f70366ce93ece1f4b52fbd6af9
Cipriano Mereu <ciprianomereu@gmail.com>
2023-04-16 20:31:13 +0200

    scroll button

--- pancake/src/Components/ButtonUP/ButtonUP.js
+++ pancake/src/Components/ButtonUP/ButtonUP.js
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
 import Button from "../UI/Buttons/Button";
 import "./buttonUP.css";
 
 function ButtonUP() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset;
+      const isVisible = currentScrollPos > 500;
+      setIsVisible(isVisible);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="buttonUP-container">
+    <div className={`buttonUP-container ${isVisible ? "visible" : "hidden"}`}>
       <a style={{ textDecoration: "none" }} href="#top">
         <Button type={"btn-square"} link={"#top"}>
           <span style={{ fontSize: "20px" }}> ▲</span>
